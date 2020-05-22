5728ff19ea69ac7dea1595adda57a466958e631b
berezkin88 <berezkin05@gmail.com>
2020-05-22 14:40:16 +0300

    refactor and rename config file

--- src/main/java/person/birch/chuckjokes/configurations/ChuckConfig.java
+++ src/main/java/person/birch/chuckjokes/configurations/ChuckConfig.java
@@ -3,10 +3,9 @@ package person.birch.chuckjokes.configurations;
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class ChuckConfig {
 
     @Bean
     public ChuckNorrisQuotes getChuckNorrisQuotes() {
